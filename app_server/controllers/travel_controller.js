8b137e71ac155be04f0831b9d2bf5039c0fb237c
gaut2172 <gaut2172@gmail.com>
2021-05-17 15:01:04 -0400

    Convert travel template from static data to dynamic JSON

--- app_server/controllers/travel_controller.js
+++ app_server/controllers/travel_controller.js
@@ -1,6 +1,13 @@
+// access local file system
+const fs = require('fs');         // expensive
+// read the JSON data file
+const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));       // not best practice, expensive
+
+// Get travel view
 const travel = (req, res) => {
     pageTitle = process.env.npm_package_description + ' - Travel';
-    res.render('travel', {title: pageTitle});
+    // render the page. pass trips variable to handlebar
+    res.render('travel', {title: pageTitle, trips});
 }
 
 module.exports = {
