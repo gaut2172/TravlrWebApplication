e640019b5ff7fead6551517739e83ef51f033f62
gaut2172 <gaut2172@gmail.com>
2021-05-18 11:25:57 -0400

    Converted all handlebar templates from static data to dynamic JSON

--- app_server/controllers/meals_controller.js
+++ app_server/controllers/meals_controller.js
@@ -1,6 +1,11 @@
+// access local file system
+const fs = require('fs');         // expensive
+// read the JSON data file
+const foods = JSON.parse(fs.readFileSync('./data/foods.json', 'utf8'));       // not best practice, expensive
+
 const meals = (req, res) => {
     pageTitle = process.env.npm_package_description + ' - Meals';
-    res.render('meals', {title: pageTitle});
+    res.render('meals', {title: pageTitle, foods});
 }
 
 module.exports = {
--- app_server/controllers/rooms_controller.js
+++ app_server/controllers/rooms_controller.js
@@ -1,6 +1,11 @@
+// access local file system
+const fs = require('fs');         // expensive
+// read the JSON data file
+const rooms_data = JSON.parse(fs.readFileSync('./data/rooms.json', 'utf8'));       // not best practice, expensive
+
 const rooms = (req, res) => {
     pageTitle = process.env.npm_package_description + ' - Rooms';
-    res.render('rooms', {title: pageTitle});
+    res.render('rooms', {title: pageTitle, rooms_data});
 }
 
 module.exports = {
